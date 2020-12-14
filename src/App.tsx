ba78c58648b23a57da9b84a4eb03f068b57a2f1b
Gabriel <perien.op@gmail.com>
2020-12-14 11:00:31 -0300

    Updates
    Criado header da aplicação e atualizado alguns componentes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import Layout from './components/Layout';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <h1>Olá</h1>
+    <>
+      <Layout />
+
+      <GlobalStyles />
+    </>
+    
   );
 }
 
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo } from './styles';
+
+const Main: React.FC = () => {
+  return(
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+
+        <ProfileInfo>
+          <strong>Gabriel Carmo</strong>
+          <span>458 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      {/* <ProfilePage /> */}
+
+      {/* <BottomMenu>
+        <HomeIcon />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu> */}
+    </Container>
+  );
+}
+
+export default Main;
