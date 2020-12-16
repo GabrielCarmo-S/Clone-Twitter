763aed68d5aba62c402433c8a425443504877938
Gabriel <perien.op@gmail.com>
2020-12-15 21:27:26 -0300

    Updates
    Criado bottom menu e estilizado para que seja responsivo e apareça só com 500px.

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Container, Header, BackIcon, ProfileInfo } from './styles';
+import { Container, Header, BackIcon, ProfileInfo, BottomMenu, HomeIcon, SearchIcon, BellIcon, EmailIcon } from './styles';
+
 
 const Main: React.FC = () => {
   return(
@@ -18,12 +19,12 @@ const Main: React.FC = () => {
 
       {/* <ProfilePage /> */}
 
-      {/* <BottomMenu>
-        <HomeIcon />
+       <BottomMenu>
+        <HomeIcon className="active" />
         <SearchIcon />
         <BellIcon />
         <EmailIcon />
-      </BottomMenu> */}
+      </BottomMenu>
     </Container>
   );
 }
