8d20be6965b07dcfe910ceabf739df261394c7b1
Gabriel <perien.op@gmail.com>
2020-12-17 10:00:39 -0300

    Updates

---
+++ src/components/Tweet/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Tweet: React.FC = () => {
+  return (
+    <Container>
+      <Retweeted>
+        <RocketseatIcon />
+        Você retweetou
+      </Retweeted>
+
+      <Body>
+        <Avatar />
+
+        <Content>
+          <Header>
+            <strong>Gabriel Carmo</strong>
+            <span>@gabrielcs</span>
+            <Dot />
+            <time>04 Ago</time>
+          </Header>
+
+          <Description>DFSDFSDFSDFSDF</Description>
+
+          <ImageContent />
+
+          <Icons>
+            <Status>
+              <CommentIcon />
+              18
+            </Status>
+            <Status>
+              <RetweetIcon />
+              18
+            </Status>
+            <Status>
+              <LikeIcon />
+              18
+            </Status>
+          </Icons>
+        </Content>
+      </Body>
+    </Container>
+  );
+}
+
+export default Tweet;
