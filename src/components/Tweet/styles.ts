0146716cb828ca1bb47b68a3dc934362e7817f00
Gabriel <perien.op@gmail.com>
2021-01-02 16:53:36 -0300

    Updates

--- src/components/Tweet/styles.ts
+++ src/components/Tweet/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 import { Chat, Rocketseat, Favorite} from '../../styles/Icons';
 
@@ -61,24 +61,122 @@ export const Content = styled.div`
   padding-left: 59px;
 `;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
 
-export const Dot = styled.div``;
+  font-size: 15px;
+  white-space: nowrap;
 
-export const Description = styled.div``;
+  > strong {
+    margin-right: 5px;
+  }
 
-export const ImageContent = styled.div``;
+  > span, time {
+    color: var(--gray);
+  }
 
-export const Icons = styled.div``;
+  > strong, span {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+`;
 
-export const Status = styled.div``;
+export const Dot = styled.div`
+  background: var(--gray);
+  width: 2px;
+  height: 2px;
+  margin: 0 10px;
+`;
 
-export const CommentIcon = styled.div``;
+export const Description = styled.p`
+  font-size: 14px;
+  margin-top: 4px;
+`;
 
-export const RetweetIcon = styled.div``;
+export const ImageContent = styled.div`
+  margin-top: 12px;
+  width: 100%;
+  height: min(285px, max(175px, 41vw));
 
-export const LikeIcon = styled.div``;
+  background: var(--outline);
+  border-radius: 14px;
 
-export const ChatIcon = styled(Chat)``;
+  cursor: pointer;
 
-export const FavoriteIcon = styled(Favorite)``;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 11px auto 0;
+  width: 100%;
+
+  @media(min-width: 330px) {
+    width: 63%;
+  }
+
+  > div{
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+`;
+
+export const Status = styled.div`
+  display: flex;
+  align-items: center;
+
+  font-size: 14px;
+
+  > svg {
+    margin-right: 5px;
+  }
+
+  &:nth-child(1) {
+    &, > svg path {
+      color: var(--gray);
+    }
+  }
+
+  &:nth-child(2) {
+    color: var(--retweet);
+
+    > svg path {
+      fill: var(--retweet);
+    }
+  }
+
+  &:nth-child(3) {
+    color: var(--like);
+
+    > svg {
+      fill: var(--like);
+    }
+  }
+`;
+
+const iconCSS = css`
+  width: 19px;
+  height: 19px;
+`;
+
+export const CommentIcon = styled(Chat)`
+  ${iconCSS}
+`;
+
+export const RetweetIcon = styled(Rocketseat)`
+  ${iconCSS}
+`;
+
+export const LikeIcon = styled(Favorite)`
+  ${iconCSS}
+`;
